7c2ee9917ca491692974d406271bfc2b64f88fda
alvinKyle <alvinkyle70@gmail.com>
2024-07-02 16:39:00 +0300

    updated

--- server/app.py
+++ server/app.py
@@ -14,13 +14,44 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods = ['GET','POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        message = Message.query.order_by(Message.created_at).all()
+        return make_response([mess.to_dict() for mess in message],200 )
+    
+    elif request.method == 'POST':
+        output = request.get_json()
+        messo = Message(
+            body = output['body'],
+            username = output['username']
+        )
+        db.session.add(messo)
+        db.session.commit()
+        res = make_response(messo.to_dict(),201)
+        return res
+    
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['PATCH','DELETE'])
 def messages_by_id(id):
-    return ''
+    messo = Message.query.filter_by(id=id).first()
+
+    if request.method ==  'PATCH':
+        output = request.get_json()
+        for attr in output:
+            setattr(messo,attr,output[attr])
+
+        db.session.add(messo)
+        db.session.commit()
+        res = make_response(messo.to_dict(),200)
+        return res
+    
+    elif request.method == 'DELETE':
+        db.session.delete(messo)
+        db.session.commit()
+
+        res = make_response({'deleted':True},200)
+        return res
 
 if __name__ == '__main__':
     app.run(port=5555)
